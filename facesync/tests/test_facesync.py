132e7e423d7818e43ee7ac04f0a3dce09431797c
jcheong <jcheong0428@gmail.com>
2017-01-19 14:43:26 -0500

    test

--- facesync/tests/test_facesync.py
+++ facesync/tests/test_facesync.py
@@ -3,3 +3,8 @@ from facesync.facesync import facesync
 def test_facesyc(tmpdir):
 	fs = facesync()
 	assert(len(fs.audio_files)==0)
+	assert(len(fs.video_files)==0)
+	assert(isinstance(fs.target_audio,None))
+	assert(isinstance(fs.audio_files),list))
+	assert(isinstance(fs.video_files),list))
+
