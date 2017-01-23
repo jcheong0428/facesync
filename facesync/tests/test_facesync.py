d6b25b81ce7e60ede18cf717e31a23b055029191
jcheong <jcheong0428@gmail.com>
2017-01-23 00:41:50 -0500

    confident

--- facesync/tests/test_facesync.py
+++ facesync/tests/test_facesync.py
@@ -11,14 +11,14 @@ def test_facesyc(tmpdir):
 	assert(isinstance(fs.video_files,list))
 	assert(isinstance(fs.offsets,list))
 	cwd = os.getcwd()
-	video_files = [os.path.join(cwd,'facesync/resources/sample1.MP4')]
-	target_audio = os.path.join(cwd,'facesync/resources/cosan_synctune.wav')
+	video_files = [os.path.join(cwd,'sample1.MP4')]
+	target_audio = os.path.join(cwd,'cosan_synctune.wav')
 	fs = facesync(video_files=video_files,target_audio=target_audio)
 	fs.extract_audio()
 	print(glob.glob(os.path.join(str(cwd.join('*')))))
 	print(glob.glob(os.path.join(str(cwd.join('facesync/*')))))
 	print(glob.glob(os.path.join(str(cwd.join('facesync/resources/*')))))
-	assert(fs.audio_files == [os.path.join(cwd,'facesync/resources/sample1.wav')])
+	assert(fs.audio_files == [os.path.join(cwd,'sample1.wav')])
 	fs.find_offset_fft()
 	assert(isinstance(fs.offsets,list))
 	
